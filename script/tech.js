579086bae9791cfaec36cc8db6b365ef7157158e
DeexithParand2k2 <deexith2002@gmail.com>
2022-08-06 10:34:33 +0530

    plus and loading animation complete

--- script/tech.js
+++ script/tech.js
@@ -1,7 +1,8 @@
 var newDoc = document.getElementById('plus');
-newDoc.addEventListener("click", function() {
-    newDoc.style.transform = 'rotate(180deg)';
-})
+
+$("#plus").click(function(){
+    $('.transform').toggleClass('transform-active');
+});
 
 var details = document.getElementsByClassName('details')[0];
 var vl = document.getElementsByClassName('vl')[0];
@@ -12,10 +13,10 @@ var upArrow = document.getElementById('upArrow');
 function displayMany(){
     details.style.display = 'flex';
     vl.style.display = 'flex';
+    bText.style.display = 'block';
     content.style.display = 'flex';
     upArrow.style.display = 'flex';
     newDoc.style.display = 'none';
-
     
     setTimeout(function(){
         setOpacity();
@@ -26,6 +27,7 @@ function setOpacity(){
     details.style.opacity = "1.0";
     vl.style.opacity = "1.0"; 
     content.style.opacity = "1.0";
+    bText.style.opacity = "1.0";
     upArrow.style.opacity = "1.0";
     newDoc.style.opacity = "1.0";
 }
@@ -34,11 +36,13 @@ function resetMany(){
     details.style.display = 'none';
     vl.style.display = 'none';
     content.style.display = 'none';
+    bText.style.display = 'none';
     upArrow.style.display = 'none';
     newDoc.style.display = 'flex';
 
     details.style.opacity = "0";
     vl.style.opacity = "0"; 
+    bText.style.opacity = "0";
     content.style.opacity = "0";
     upArrow.style.opacity = "0";
 }
