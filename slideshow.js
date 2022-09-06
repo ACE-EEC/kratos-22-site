6c4cdbefc5307af930568d52bf4c5eb4a6041fa5
Nithish Kumar <nithssh@protonmail.com>
2022-09-06 20:54:54 +0530

    Add sliding animation (causes flickering)

--- slideshow.js
+++ slideshow.js
@@ -1,17 +1,60 @@
 const slideCount = 4;
 let index = 0;
+let leftSlide = document.getElementById('previousSlide');
 let activeSlide = document.getElementById('activeSlide');
+let rightSlide = document.getElementById('nextSlide');
+
+const animRightToLeft = [
+  { transform: 'translate(0%)' },
+  { transform: 'translate(-100%)' }
+];
+
+const animLeftToRight = [
+  { transform: 'translate(0%)' },
+  { transform: 'translate(100%)' }
+];
+
+const animOptions = {
+  duration: 600,
+  delay: 0,
+  iterations: 1,
+  easing: 'ease'
+}
 
 function nextSlide() {
-  index = (index + 1) % slideCount;
-  activeSlide.src = `public/images/slide-${index}.jpg`;
+  activeSlide.animate(animRightToLeft, animOptions);
+  rightSlide.animate(animRightToLeft, animOptions);
+
+  setTimeout(() => {
+    index = nextCyclicIndex(index);
+    activeSlide.src = `public/images/slide-${index}.jpg`;
+    leftSlide.src = `public/images/slide-${prevCyclicIndex(index)}.jpg`;
+    rightSlide.src = `public/images/slide-${nextCyclicIndex(index)}.jpg`;
+  }, animOptions.duration);
 }
 
 function previousSlide() {
-  if (index) {
-    index = (index - 1) % slideCount;
-  } else {
-    index = slideCount - 1;
+  leftSlide.animate(animLeftToRight, animOptions);
+  activeSlide.animate(animLeftToRight, animOptions);
+
+  setTimeout(() => {
+    index = prevCyclicIndex(index);
+    activeSlide.src = `public/images/slide-${index}.jpg`;
+    leftSlide.src = `public/images/slide-${prevCyclicIndex(index)}.jpg`;
+    rightSlide.src = `public/images/slide-${nextCyclicIndex(index)}.jpg`;
+  }, animOptions.duration);
+}
+
+function prevCyclicIndex(n) {
+  if (n === 0) {
+    return slideCount - 1;
   }
-  activeSlide.src = `public/images/slide-${index}.jpg`;
+  return n - 1;
+}
+
+function nextCyclicIndex(n) {
+  if (n === slideCount - 1) {
+    return 0;
+  }
+  return n + 1;
 }
