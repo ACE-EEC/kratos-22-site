8036a0b1c0432543d399755a3cff518eb1d1996d
Nithish Kumar <nithssh@protonmail.com>
2022-10-11 16:17:03 +0530

    Change table styling in retrieve page

--- scripts/retrieve.js
+++ scripts/retrieve.js
@@ -18,28 +18,28 @@ function renderForms(forms) {
     let teamEvents = forms[i].team_events.map((x, j, a) => `
       ${j == 0
         ? `<td>${x}</td> 
-           <td>${forms[i][x].full_name}, ${forms[i][x].member1_full_name || '_'}, ${forms[i][x].member2_full_name || '_'}, ${forms[i][x].member3_full_name || '_'}</td>`
+           <td class='col-darker'>${forms[i][x].full_name}, ${forms[i][x].member1_full_name || '_'}, ${forms[i][x].member2_full_name || '_'}, ${forms[i][x].member3_full_name || '_'}</td>`
         : `<tr>
             <td>${x}</td>
-            <td>${forms[i][x].full_name}, ${forms[i][x].member1_full_name || '_'}, ${forms[i][x].member2_full_name || '_'}, ${forms[i][x].member3_full_name || '_'}</td>
+            <td class='col-darker'>${forms[i][x].full_name}, ${forms[i][x].member1_full_name || '_'}, ${forms[i][x].member2_full_name || '_'}, ${forms[i][x].member3_full_name || '_'}</td>
           </tr>`}
     `)
 
     if (!tl) {
-      teamEvents = `<td></td><td></td>`
+      teamEvents = `<td></td><td class='col-darker'></td>`
     }
 
     $('#tbody').append(`
       <tr rowspan="${tl || 1}">
         <td rowspan="${tl || 1}">${forms[i].full_name}</td>
-        <td rowspan="${tl || 1}">${forms[i].college_name}</td>
+        <td rowspan="${tl || 1}" class='col-darker'>${forms[i].college_name}</td>
         <td rowspan="${tl || 1}">${forms[i].email}</td>
-        <td rowspan="${tl || 1}">${forms[i].mobile}</td>
+        <td rowspan="${tl || 1}" class='col-darker'>${forms[i].mobile}</td>
         <td rowspan="${tl || 1}">${forms[i].screenshot
         ? `<a href="${forms[i].screenshot}"target="_blank" rel="noopener noreferrer">Link</a></td>`
         : 'none'
       }
-        <td rowspan="${tl || 1}">${forms[i].solo_events.length == 0
+        <td rowspan="${tl || 1}" class='col-darker'>${forms[i].solo_events.length == 0
         ? ''
         : forms[i].solo_events
       }</td>
